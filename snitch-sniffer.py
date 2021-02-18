53b51559f75b984230f0bcdce7eaeb07f94411f9
Dale <aplodontia@icloud.com>
2021-02-17 23:29:41 -0800

    Add files via upload

---
+++ snitch-sniffer.py
@@ -0,0 +1,7 @@
+This is where my mythod code would go for this "snitch-sniffer" project. For now, I'll just copy the text from my README file:
+# The Golden Snitch Sniffer
+This is a project that uses multiple long-range ultrasonic sensors 
+to find and track an object flying in three-dimensional space. 
+It displays the object's coordinates, speed, and trajectory 
+through a VR headset.
+I'm doing some new work on the 'find function' for this project. It will be complicated but I should be able to find some code in other github projects to help. 
